692eee5586e6ec861945696719436e19b38d2592
8dgkim <0dgkim@gmail.com>
2021-12-10 09:10:27 +0900

    component common

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Header from '@components/common/Header'
+import Footer from '@components/common/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>AnonGround</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header title="AnonGround coming soon...!" />
+      
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Currently editing <code>pages/index.js</code>
         </p>
         <p>
-          
         </p>
       </main>
 
